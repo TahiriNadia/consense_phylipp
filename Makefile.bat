01424809cf771f9467c1fed24f2b2fc2820a4f74
Nadia Tahiri <tahiri.nadia@courrier.uqam.ca>
2016-07-04 11:47:03 -0400

    Update Makefile.bat

--- Makefile.bat
+++ Makefile.bat
@@ -1,4 +1,5 @@
 cc -O3 -fomit-frame-pointer   -c -o phylip.o phylip.c
 cc -O3 -fomit-frame-pointer   -c -o consense.o consense.c
+cc -O3 -fomit-frame-pointer   -c -o main.o main.c
 cc -O3 -fomit-frame-pointer   -c -o cons.o cons.c
-cc -O3 -fomit-frame-pointer consense.o phylip.o cons.o -lm -o consense
+cc -O3 -fomit-frame-pointer consense.o phylip.o cons.o main.o -lm -o consense
